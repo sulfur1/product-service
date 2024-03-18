ffa2b95492705ee171629afbb8a1c6718a8a1082
AlexandrLezh <sasha_l@inbox.lv>
2024-03-18 09:43:59 +0200

    #17 Cucumber setup and checkstyle fine-tuning
    Changes for pom Checkstyle and Cucumber setup

--- e2e/src/main/java/com/iprodi08/productservice/App.java
+++ e2e/src/main/java/com/iprodi08/productservice/App.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public final class App {
-    private App() {
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
